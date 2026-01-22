0f594112c64352cb0952697dabc99af4118ab6ad
divya-garg <dgarg17306@gmail.com>
2026-01-22 22:47:33 +0530

    Add CommandParser interface for CLI command parsing

--- CommandParser.h
+++ CommandParser.h
@@ -2,3 +2,21 @@
 #define COMMANDPARSER_H
 #include <string>
 #include <vector>
+using namespace std;
+enum class CommandType{
+VIEW,
+APPEND,
+SEARCH,
+DELETE,
+EXIT,
+INVALID
+};
+
+class CommandParser {
+public:
+    CommandType parseCommand(
+        const string& input,
+        vector<string>& tokens
+    ) const;
+};
+#endif
